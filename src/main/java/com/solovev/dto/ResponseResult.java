395d24fc18754841b1c3d6e4429c13ba1a784da9
Solovev Anton <solovev.anton93@gmail.com>
2023-10-12 18:52:57 +0200

    added response result DTO

---
+++ src/main/java/com/solovev/dto/ResponseResult.java
@@ -0,0 +1,27 @@
+package com.solovev.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Class used to get results from server
+ * @param <T>
+ */
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseResult<T> {
+    private String message;
+    private T data;
+    /**
+     * Represents object as a Json sting
+     * @return string in Json format of this object
+     */
+    public String jsonToString() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        return objectMapper.writeValueAsString(this);
+    }
+}
