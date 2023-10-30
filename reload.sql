5df88fd58082014079977b8dfb598da2a56ff92d
Solovev Anton <solovev.anton93@gmail.com>
2023-10-30 19:21:49 +0100

    added index and reload options

---
+++ reload.sql
@@ -0,0 +1,18 @@
+INSERT INTO users (login, name, password, registration_date)
+VALUES
+    (1, 'firstLog', 'firstPass', NOW()),
+    (2, 'secondLog', 'secondPass', NOW()),
+    (3, 'thirdLog', 'thirdPass', NOW());
+
+INSERT INTO categories (id, name, user_id)
+VALUES
+    (1, 'firstCat', 1),
+    (2, 'secondCat', 1),
+    (3, 'thirdCat', 2);
+
+INSERT INTO cards (id, question, answer, category_id, creation_date)
+VALUES
+    (1, 'Q1', 'A1', 1, NOW()),    -- Assuming 'Q1' and 'A1' are linked to the category with ID 1
+    (2, 'Q2', 'A2', 1, NOW()),    -- Assuming 'Q2' and 'A2' are also linked to the category with ID 1
+    (3, 'Q3', 'A3', 2, NOW());
+
