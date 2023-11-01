4d99d14b6ee2a1749b8067bea6b4c62743733faf
Solovev Anton <solovev.anton93@gmail.com>
2023-11-01 20:48:39 +0100

    added registration form

---
+++ src/main/webapp/scripts/registration.js
@@ -0,0 +1,6 @@
+function clearForm(){
+    document.getElementById("userName").value = "";
+    document.getElementById("login").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("confirmPassword").value = "";
+}
