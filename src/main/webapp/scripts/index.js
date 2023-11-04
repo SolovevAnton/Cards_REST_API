1db8f5b99b7a1783787fed1e701e77b951a12eff
Solovev Anton <solovev.anton93@gmail.com>
2023-11-04 11:16:42 +0100

    added index js

---
+++ src/main/webapp/scripts/index.js
@@ -0,0 +1,30 @@
+let user_id = 1;
+
+function fillTableCardsForUser(){
+    $("cardsTableBody").html("");
+    $.ajax({
+        type: "GET",
+        url: `cards?userId=${user_id}`,
+        success: [function (result) {
+            let cards = result.data;
+            for (let i = 0; i < cards.length; i++) {
+                let markup = "<tr>" +
+                    "<td>" + cards[i].question + "</td>" +
+                    "<td>" + cards[i].answer + "</td>" +
+                    "<td>" + cards[i].category.name + "</td>" +
+                    "<td>" + cards[i].creationDate + "</td>" +
+                    "</tr>";
+
+                $("table tbody").append(markup);
+            }
+        }],
+        error: [function (e) {
+            alert("error");
+            alert(JSON.stringify(e));
+        }]
+    });
+}
+
+$(document).ready(function (){
+    fillTableCardsForUser();
+});
